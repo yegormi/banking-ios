a3f3fd705359a3621184339e5d1b348eec0fa0d4
yegormi <egormiropoltsev79@gmail.com>
2024-06-02 11:43:32 +0300

    feat: implement font styleguide

---
+++ Sources/Styleguide/Fonts.swift
@@ -0,0 +1,55 @@
+import SwiftUI
+
+public extension Font {
+    static var appHeadingXL: Self {
+        .system(size: 15, weight: .bold)
+    }
+
+    static var appHeading1: Self {
+        .system(size: 28, weight: .bold)
+    }
+
+    static var appHeading2: Self {
+        .system(size: 22, weight: .bold)
+    }
+
+    static var appHeading3: Self {
+        .system(size: 17, weight: .bold)
+    }
+
+    static var appHeading4: Self {
+        .system(size: 15, weight: .bold)
+    }
+
+    static var appLabelLargeBold: Self {
+        .system(size: 18, weight: .bold)
+    }
+
+    static var appLabelMediumBold: Self {
+        .system(size: 15, weight: .bold)
+    }
+
+    static var appLabelSmallBold: Self {
+        .system(size: 13, weight: .bold)
+    }
+
+    static var appLabelXSBold: Self {
+        .system(size: 12, weight: .bold)
+    }
+
+    static var appParagraphLg: Self {
+        .system(size: 18, weight: .regular)
+    }
+
+    static var appParagraphRegular: Self {
+        .system(size: 16, weight: .regular)
+    }
+
+    static var appParagraphSmall: Self {
+        .system(size: 14, weight: .regular)
+    }
+
+    static var appParagraphXS: Self {
+        .system(size: 12, weight: .regular)
+    }
+}
